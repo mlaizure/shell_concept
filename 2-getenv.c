d3f1101931a9c1b1c087feddbef16b249c5b74c6
mlaizure <maddi.laizure@gmail.com>
2020-11-15 23:12:16 +0000

    Task 2 getenv

--- 2-getenv.c
+++ 2-getenv.c
@@ -1,50 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/*
-char **split_env(char *);
-*/
+extern char **environ;
+
+char **split_delim(char *, const char *);
 char * _getenv(const char *name);
 
-int main(__attribute__((unused)) int ac, __attribute__((unused)), char *av[], char **env)
+int main(void)
 {
-	char *PATH;
+	char *PATH, *HOME, *DNE;
 
 	PATH = _getenv("PATH");
+	HOME = _getenv("HOME");
+	DNE = _getenv("DNE");
 	printf("PATH: %s\n", PATH);
+	printf("HOME: %s\n", HOME);
+	printf("DNE: %s\n", DNE);
+	return (0);
 }
 
 char * _getenv(const char *name)
 {
 	int i = 0;
+	char **n_v, *n;
 
-	
-
-/*
-char **split_env(char *my_env)
-{
-	char *my_env1, *env_var;
-	int i = 0, j = 0;
-	char **full_env;
-
-	my_env1 = strdup(my_env);
-	env_var = strtok(line, ":");
-
-	while (env_var)
+	for (i = 0; environ[i]; ++i)
 	{
-		i++;
-		env_var = strtok(NULL, "\0");
+		n_v = split_delim(environ[i], "=");
+		n = n_v[0];
+		if (!strcmp(n, name))
+			return (strstr(environ[i], "=") + 1);
 	}
-
-	full_env = malloc(sizeof(char *) * (i + 1));
-
-	env_var = strtok(my_env1, "\0");
-
-	while (env_var)
-	{
-		array[j++] = env_var;
-		env_var = strtok(NULL, "\0");
-	}
-
-	return (full_env);
+	return (NULL);
 }
-*/
+
+char **split_delim(char *s, const char *delim)
+{
+	char *s1, *piece, *ptr;
+	int i = 0, j = 0;
+	char **result;
+
+	for (i = 0, ptr = s; (ptr = strstr(ptr, delim)); ++i, ++ptr)
+		;
+
+	result = malloc(sizeof(char *) * (i + 1));
+
+	s1 = strdup(s);
+
+	piece = strtok(s1, delim);
+
+	while (piece)
+	{
+		result[j++] = piece;
+		piece = strtok(NULL, delim);
+	}
+
+	return (result);
+}
